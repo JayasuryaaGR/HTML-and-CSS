bb7b405f0b2a03f769e8d9f83605d74b9852f90f
Jayasuryaa G R <66047413+JayasuryaaGR@users.noreply.github.com>
2020-08-22 14:50:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+body{width:100%;
+height:100%;
+margin:0px;
+}
+
+a{text-decoration:none;
+background-color:transparent;
+color:#ededed;
+}
+
+
+.header{background-color:#000;
+ color:#fff;
+ border-color: #080808;
+ min-height: 50px;
+ border: 1px solid transparent;
+}
+
+
+.inner-header{width:80%;
+ margin:auto;
+}
+
+
+.logo{float: left;
+ height: 50px;
+ padding: 15px;
+ font-size: 20px;
+ font-weight: bold;
+}
+
+
+.header-link{float:right;
+ font-size:14px;
+ height: 50px;
+ padding: 15px 15px;
+ font-size:16px;
+ font-weight: bold;
+}
+
+
+.content{min-height: 600px;}
+
+
+.banner-image{padding-bottom: 50px;
+ margin-bottom: 20px;
+ text-align: center;
+ color: #f8f8f8;
+background-image: url("file:///C:/Users/Admin/Downloads/hajs.jpg");
+background-size: cover;
+height:600px;
+}
+
+
+.inner-banner-image{padding-top: 23%;
+ width:50%;
+ margin:auto;
+height:800px;
+}
+
+
+.banner_content{position: relative;
+ padding-top: 10%;
+ padding-bottom: 10%;
+ overflow:hidden;
+ margin-bottom: 12%;
+ background-color: rgba(0, 0, 0, 0.7);
+ max-width: 660px;
+}
+
+
+.button{color: #fff;
+ background-color: #c9302c;
+ border-color: #ac2925;
+ box-shadow: inset 0 3px 5px rgba(0, 0, 0, .125);
+ padding: 10px 16px;
+ font-size: 18px;
+ border-radius: 6px;
+}
+
+
+.container{width:90%;
+ margin:auto;
+ overflow:hidden;
+}
+
+
+.items{width:31%;
+ display: block;
+ padding: 2px;
+ margin-bottom: 20px;
+ line-height: 1.42857143;
+ background-color: #fff;
+ border: 1px solid #ddd;
+ border-radius: 4px;
+ float:left;
+ margin-left:1%;
+}
+
+
+.thumbnail{display: block;
+ max-width: 100%;
+ height: auto;
+}
+
+
+.caption{color:#000;
+ padding:0px 0px 10px 0px;
+ font-weight: bold;
+ text-align: center;
+font-size:15px;
+}
+
+
+.footer{background-color: #000;
+ color:#fff;
+ font-size:14px;
+padding:3px;
+}
